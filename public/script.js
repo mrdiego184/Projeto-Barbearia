872f271abbc609ffe4ebaa0d04283b54b45db7b8
mrdiego <mrdiego184@gmail.com>
2025-07-08 11:32:58 -0300

    Update script.js
    Add popup functions

--- public/script.js
+++ public/script.js
@@ -1,3 +1,4 @@
+// script para que as sections apareçam na página quando entrarem no campo de visão da tela.
 window.location.hash = '.inicio';
 const myObserver= new IntersectionObserver ((entradas) => {
     entradas.forEach((entrada) => {
@@ -9,3 +10,25 @@ const myObserver= new IntersectionObserver ((entradas) => {
 const elementos = document.querySelectorAll('.hidden')
 
 elementos.forEach( (elemento) => myObserver.observe(elemento))
+
+// popup das assinaturas (ficará no site por um tempo, durante o lançamento do plano das assinaturas da barbearia)
+
+let popup = document.querySelector("#popup")
+
+let fechar = document.querySelector("#fechar")
+let assinar = document.querySelector("#assinaraba")
+
+function mostrarPopup (){
+    popup.style.display = "flex";
+}
+function fecharPopup () {
+    popup.style.display = "none";
+    
+}
+
+
+assinar.addEventListener("click",fecharPopup)
+fechar.addEventListener ("click", fecharPopup)
+
+
+window.onload = mostrarPopup;
